fa11dbf5b385ceafd60b465722f97aafd624767b
DrRico <851774781@qq.com>
2021-03-04 08:47:38 +0800

    2021.3.4 - restart coding

--- src/com/springjobhunting/newcoder/majorityElement.java
+++ src/com/springjobhunting/newcoder/majorityElement.java
@@ -5,5 +5,23 @@ package com.springjobhunting.newcoder;
  * @date 2021/3/4 8:42
  */
 public class majorityElement {
-
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,2,2,2,5,4,2};
+        System.out.println(fun(arr));
+    }
+    static private int fun(int[] arr){
+        int max = 1, cur = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] == cur){
+                max ++;
+            } else {
+                max --;
+            }
+            if (max == 0){
+                cur = arr[i];
+            }
+        }
+        if (max != 0) return cur;
+        else return -1;
+    }
 }
